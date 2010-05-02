a100cc1c60c11915fc6f3bed0baae304ec0a9819
Piotr Pokora <piotrek.pokora@gmail.com>
2010-05-02 16:20:36 +0000

    INITIAL. Based on gtkmm skeleton.

---
+++ libmidgard2mm/libmidgard2/libmidgard2mm.h
@@ -0,0 +1,47 @@
+/* Copyright (c) 2010  Joe Hacker <joe@example.com>
+ *
+ * This file is part of libmidgard2mm.
+ *
+ * libmidgard2mm is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 2.1 of the License,
+ * or (at your option) any later version.
+ *
+ * libmidgard2mm is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef LIBMIDGARD2MM_H_INCLUDED
+#define LIBMIDGARD2MM_H_INCLUDED
+
+/** @mainpage libmidgard2mm Reference Manual
+ *
+ * @section description Description
+ *
+ * The libmidgard2mm C++ binding provides a C++ interface on top of the libmidgard2
+ * C library.
+ *
+ * @section overview Overview
+ *
+ * [...]
+ *
+ * @section use Use
+ *
+ * To use libmidgard2mm in your C++ application, include the central header file
+ * <tt>\<libmidgard2mm.h\></tt>.  The libmidgard2mm package ships a @c pkg-config
+ * file with the correct include path and link command-line for the compiler.
+ */
+
+#include <libmidgard2mmconfig.h>
+#include <libmidgard2mm/libmidgard2.h>
+
+/** @example example/example.cc
+ * A libmidgard2mm example program.
+ */
+
+#endif /* !LIBMIDGARD2MM_H_INCLUDED */
